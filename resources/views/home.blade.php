ff36a9a8edf2502732a331c0b61f662eb883ccf1
muhamadilham701@gmail.com <muhamadilham701@gmail.com>
2021-07-19 21:10:00 +0700

    project new

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.app')
+
+@section('content')
+<!-- Content Header (Page header) -->
+<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">BELAJAR LARAVEL</h1>
+            <h1 class="m-0 text-dark">MUHAMAD ILHAM</h1>
+            <h1 class="m-0 text-dark">1101181136</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active"> UAS LARAVEL</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+        <!-- /.content -->
+    @endsection
